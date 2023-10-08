fe4c2726628c7675d1ef31ccd5a6bae15e50f8c6
j <77560326+Jxl-s@users.noreply.github.com>
2023-10-08 19:06:26 -0400

    feat: buttons make sounds

--- src/Components/ColorSwitch.tsx
+++ src/Components/ColorSwitch.tsx
@@ -9,11 +9,24 @@ import {
 import { useRef } from "react";
 import { RoundedBox } from "@react-three/drei";
 import { Colors, usePlayerStore } from "../Stores/usePlayerStore";
+import gsap from "gsap";
 
 interface Props {
     start: [number, number, number];
     color: Colors;
 }
+
+const enterSound = new Audio("/sounds/button_enter.mp3");
+enterSound.volume = 0.5;
+
+const exitSound = new Audio("/sounds/button_exit.mp3");
+exitSound.volume = 0.25;
+
+const playSound = (sound: HTMLAudioElement) => {
+    sound.play();
+
+    console.log("play sound");
+};
 
 export function ColorSwitch({ start, color }: Props) {
     const buttonRef = useRef<RapierRigidBody>(null);
@@ -28,35 +41,46 @@ export function ColorSwitch({ start, color }: Props) {
         return true;
     };
 
+    const animateButton = (y: number) => {
+        if (!buttonRef.current) return;
+
+        // Animate the button
+        const btnTr = buttonRef.current.translation();
+        const onStep = () => buttonRef.current?.setTranslation(btnTr, true);
+        gsap.to(btnTr, { y, duration: 0.1, ease: "power1.out", onUpdate: onStep }).play();
+    };
+
     const onEnter = (payload: IntersectionEnterPayload) => {
         if (!buttonRef.current) return;
         if (!isPlayerCollision(payload)) return;
 
-        // Move the button down
-        buttonRef.current.setTranslation({ x: start[0], y: start[1] - 0.1, z: start[2] }, true);
-
         // Check for colors, change colors
-        if (playerColor !== color) {
-            setPlayerColor(color);
-        }
+        if (playerColor !== color) setPlayerColor(color);
+
+        // Move the button down and play sound
+        animateButton(start[1] - 0.1);
+        playSound(enterSound);
     };
 
     const onExit = (payload: IntersectionExitPayload) => {
         if (!buttonRef.current) return;
         if (!isPlayerCollision(payload)) return;
 
-        // Move the button up
-        buttonRef.current.setTranslation({ x: start[0], y: start[1], z: start[2] }, true);
+        // Move the button up and play sound
+        animateButton(start[1]);
+        playSound(exitSound);
     };
 
     return (
         <>
+            {/* Base */}
             <RigidBody type="fixed" position={start}>
                 <RoundedBox args={[2, 1, 2]} radius={0.1}>
                     <meshStandardMaterial color="white" />
                 </RoundedBox>
             </RigidBody>
 
+            {/* Button */}
             <RigidBody ref={buttonRef} type="kinematicPosition" position={start}>
                 <RoundedBox args={[1.5, 1.5, 1.5]} radius={0.1}>
                     <meshStandardMaterial
@@ -67,10 +91,10 @@ export function ColorSwitch({ start, color }: Props) {
                 </RoundedBox>
             </RigidBody>
 
-            {/* Button detector */}
+            {/* Detector */}
             <CuboidCollider
                 sensor={true}
-                args={[0.75, 1.3, 0.75]}
+                args={[0.5, 1, 0.5]}
                 position={start}
                 onIntersectionEnter={onEnter}
                 onIntersectionExit={onExit}
